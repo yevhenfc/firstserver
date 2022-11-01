948fb82962ed6245a0e7b97edf95deb6e527264d
yevhenfc <yevhenfc@gmail.com>
2022-11-01 21:58:29 +0200

    cors added

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = require('./router.js');
+const cors = require('cors');
 const {STATIC_PATH} = require('./constants.js');
 
 const app = express();
 
+var corsOption = {origin: '*'};
+
+app.use(cors(corsOption));
 app.use(express.static(STATIC_PATH));
 app.use(express,json());
 app.use('/api',router);
